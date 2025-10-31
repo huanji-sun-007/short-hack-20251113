73a3e5f3c54c0fd6337ef0533c4c7cbec9745ca1
Miron Sadziak <mironsadziak@microsoft.com>
2025-10-31 08:41:22 +0000

    fix

--- SHORTHACK-AGENT-FRAMEWORK/deploy.sh
+++ SHORTHACK-AGENT-FRAMEWORK/deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Deploys AI Foundry and retrieves environment variables for AI endpoints and key
 
+set -e
 azd provision
 azd env get-values > .env
-. load_env.sh
 
+AZURE_RESOURCE_GROUP=$(azd env get-value AZURE_RESOURCE_GROUP)
+AZURE_OPENAI_ACCOUNT_NAME=$(azd env get-value AZURE_OPENAI_ACCOUNT_NAME)
 AZURE_OPENAI_API_KEY=$(az cognitiveservices account keys list -g $AZURE_RESOURCE_GROUP -n $AZURE_OPENAI_ACCOUNT_NAME --query "key1" -o tsv)
 echo "AZURE_OPENAI_API_KEY=\"$AZURE_OPENAI_API_KEY\"" >> .env
