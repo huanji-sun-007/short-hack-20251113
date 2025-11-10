98b972af7bb8929b9f19b4000d526a605fce8e6e
Miron Sadziak <mironsadziak@microsoft.com>
2025-11-10 07:28:19 +0000

    fixes

--- SHORTHACK-AGENT-FRAMEWORK/deploy.sh
+++ SHORTHACK-AGENT-FRAMEWORK/deploy.sh
@@ -11,8 +11,10 @@ AZURE_OPENAI_ACCOUNT_NAME=$(azd env get-value AZURE_OPENAI_ACCOUNT_NAME)
 AZURE_OPENAI_API_KEY=$(az cognitiveservices account keys list -g $AZURE_RESOURCE_GROUP -n $AZURE_OPENAI_ACCOUNT_NAME --query "key1" -o tsv)
 echo "AZURE_OPENAI_API_KEY=\"$AZURE_OPENAI_API_KEY\"" >> .env
 
+# Assign roles to the signed-in user to be able to create agents within AI Foundry, necessary for cases using AzureAIAgentClient
 AZURE_SUBSCRIPTION_ID=$(azd env get-value AZURE_SUBSCRIPTION_ID)
 AZURE_PRINCIPAL_ID=$(az ad signed-in-user show --query id -o tsv)
-
 az role assignment create --assignee $AZURE_PRINCIPAL_ID --role "Azure AI Developer" --scope /subscriptions/$AZURE_SUBSCRIPTION_ID/resourceGroups/$AZURE_RESOURCE_GROUP
 az role assignment create --assignee $AZURE_PRINCIPAL_ID --role "Azure AI User" --scope /subscriptions/$AZURE_SUBSCRIPTION_ID/resourceGroups/$AZURE_RESOURCE_GROUP
+
+echo "Deployment complete. Environment variables saved to .env file."
